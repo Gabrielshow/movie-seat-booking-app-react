c2255473d7b8aa66791140aedf79a04ecb655b61
Gabrielshow <adefelashogbanmu@gmail.com>
2023-08-05 21:09:06 +0100

    first commit

---
+++ components/Seat.js
@@ -0,0 +1,9 @@
+import React from "react;"
+
+const Seat = ({type, handleSelect}) => {
+  return (
+    <div onClick={handleSelect} className={`seat ${type}`}></div>
+  );
+}
+
+export default Seat;
---
+++ src/App.js
@@ -0,0 +1,105 @@
+import React , {useState} from "react";
+import Seat from "../components/Seat";
+import MovieSelector from "../components/MovieSelector";
+import "./App.css";
+
+const App = () => {
+  const [type, setType] = useState("");
+  const [count, setCount] = useState(0);
+  const handleSelect = () => {
+    setCount(count + 1);
+    setType("occupied");}
+  return (
+    <>
+      <div>
+        <MovieSelector />
+        <ul className="showcase">
+          <li>
+            <div className="seat"></div>
+            <small>N/A</small>
+          </li>
+
+          <li>
+            <div className="seat selected"></div>
+            <small>Selected</small>
+          </li>
+
+          <li>
+            <div className="seat occupied"></div>
+            <small>Occupied</small>
+          </li>
+        </ul>
+        <div className="container">
+          <div className="screen"></div>
+          <div className="row">
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+          </div>
+          <div className="row">
+            <Seat handleSelect={handleSelect} />
+            <Seat handleSelect={handleSelect} />
+            <Seat handleSelect={handleSelect} />
+            <Seat type={"occupied"} />
+            <Seat type={"occupied"} />
+            <Seat handleSelect={handleSelect} />
+            <Seat handleSelect={handleSelect} />
+            <Seat handleSelect={handleSelect} />
+          </div>
+          <div className="row">
+            <Seat handleSelect={handleSelect} />
+            <Seat handleSelect={handleSelect} />
+            <Seat handleSelect={handleSelect} />
+            <Seat handleSelect={handleSelect} />
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat type={type} />
+            <Seat type={type} />
+          </div>
+          <div className="row">
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+          </div>
+          <div className="row">
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat type={type} />
+            <Seat type={type} />
+          </div>
+          <div className="row">
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat handleSelect={handleSelect}/>
+            <Seat type={type} />
+            <Seat type={type} />
+            <Seat type={type} />
+            <Seat handleSelect={handleSelect}/>
+          </div>
+        </div>
+      </div>
+
+      <p class="text">
+        You have selected <span id="count">count</span> seats for a price of $
+        <span id="total">0</span>
+      </p>
+    </>
+  );
+};
+
+export default App;
